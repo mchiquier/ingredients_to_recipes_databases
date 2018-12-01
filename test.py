3762fc0b8928e7f4b6d221e754635c06c6c58166
Mia Chiquier <miachiquier@huntsman-ve503-0724.apn.wlan.upenn.edu>
2018-12-01 12:36:27 -0500

    adding id_to_id

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 import pickle
+import csv 
+import jsonlines 
+import tqdm 
 
 with open('correspondance.pickle', 'rb') as handle:
     unserialized_data = pickle.load(handle)
 
-print(unserialized_data)
+with jsonlines.open("output.ndjson") as reader:
+
+    with open('id_to_id.csv', mode='w') as f:
+        id_to_id = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for obj in reader:
+            recipe_id = obj["recipe_id"]
+            ingredient = obj['INGREDIENT']
+            mydict = unserialized_data[ingredient]
+            data_id = mydict['code']
+            id_to_id.writerow([recipe_id,data_id])
