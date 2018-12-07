a2b69fd60cb1107e51883469d3f8307b64970ab9
Andre Wallace <waandre@seas.upenn.edu>
2018-12-07 16:39:43 -0500

    updated data

---
+++ final_data/parserFiles/jsonToCSV.py
@@ -0,0 +1,52 @@
+import json
+import csv
+
+fn = open("../outputData/nutrients.json")
+fnr = fn.read()
+
+nn = open("../outputData/ingredients.json")
+nnr = nn.read()
+
+rn = open("../outputData/recipes.json")
+rnr = rn.read()
+
+nutrients = json.loads(fnr)
+ingredients = json.loads(nnr)
+recipes = json.loads(rnr)
+
+nutArr = ["nid", "name", "protein (g)", "fat (g)", "energy (kcal)",
+               "sugar (g)", "calcium (g)", "potassium (g)", "cholesterol (g)", "carbohydrates (g)",
+               "fiber (g)", "sodium (g)", "iron (g)"]
+ingArr = ["iid", "rid", "title", "ingredient"]
+recArr = ["rid", "title", "rating", "instructions", "calories", "description"]
+
+
+
+ingreds = csv.writer(open('../outputData/ingredients.csv', "w"))
+recs = csv.writer(open('../outputData/recipes.csv', "w"))
+
+ingreds.writerow(ingArr)
+recs.writerow(recArr)
+with open("../outputData/nutrients.csv", 'w') as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=',')
+    csvWriter.writerow(nutArr)
+    for el in nutrients:
+        csvWriter.writerow([el[nutArr[0]], el[nutArr[1]], el[nutArr[2]], el[nutArr[3]],
+                      el[nutArr[4]], el[nutArr[5]], el[nutArr[6]], el[nutArr[7]],
+                           el[nutArr[8]], el[nutArr[9]], el[nutArr[10]], el[nutArr[11]],
+                           el[nutArr[12]]])
+
+
+with open("../outputData/ingredients.csv", 'w') as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=',')
+    csvWriter.writerow(ingArr)
+    for el in ingredients:
+        ingreds.writerow([el[ingArr[0]], el[ingArr[1]], el[ingArr[2]], el[ingArr[3]]])
+
+
+with open("../outputData/recipes.csv", 'w') as my_csv:
+    csvWriter = csv.writer(my_csv, delimiter=',')
+    csvWriter.writerow(recArr)
+    for el in recipes:
+        recs.writerow([el[recArr[0]], el[recArr[1]], el[recArr[2]], el[recArr[3]],
+                      el[recArr[4]], el[recArr[5]]])
---
+++ final_data/parserFiles/recipeParser.py
@@ -0,0 +1,37 @@
+import json
+import collections
+
+file = open("./rawData/epicuriousData/full_format_recipes.json")
+jstr = file.read()
+data = json.loads(jstr)
+
+headerT1 = ["rid", "title", "rating", "instructions", "calories", "description"]
+dataT1 = list()
+
+headerT2 = ["iid","rid", "title", "ingredient"]
+dataT2 = list()
+
+idCount = 0
+rid = 0
+for item in data:
+    rating = item["rating"] if "rating" in item else -1.0
+    title = item['title'] if "title" in item else ""
+    ingreds = item['ingredients'] if "ingredients" in item else []
+    directions = item['directions'] if "directions" in item else []
+    calories = item['calories'] if 'calories' in item else -1.0
+    description = item['desc'] if 'desc' in item else ""
+    for ingred in ingreds:
+        temp = [rid, idCount, title, ingred]
+        dataT2.append(collections.OrderedDict(zip(headerT2, temp)))
+        rid += 1
+    arr = [idCount,title, rating, directions, calories, description]
+    dataT1.append(collections.OrderedDict(zip(headerT1, arr)))
+    idCount += 1
+
+
+with open("./outputData/recipes.json", 'w') as file:
+    json.dump(dataT1, file, indent=2)
+
+with open("./outputData/ingredients.json", 'w') as file:
+    json.dump(dataT2, file, indent=2)
+
