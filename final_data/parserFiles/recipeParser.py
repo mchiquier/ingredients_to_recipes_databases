e28ec460cfd53ca9e92e6cae983314a7fce23349
Andre Wallace <waandre@seas.upenn.edu>
2018-12-08 15:05:17 -0600

    fixed parser files and ingreds

--- final_data/parserFiles/recipeParser.py
+++ final_data/parserFiles/recipeParser.py
@@ -1,7 +1,7 @@
 import json
 import collections
 
-file = open("./rawData/epicuriousData/full_format_recipes.json")
+file = open("../rawData/epicuriousData/full_format_recipes.json")
 jstr = file.read()
 data = json.loads(jstr)
 
@@ -18,20 +18,23 @@ for item in data:
     title = item['title'] if "title" in item else ""
     ingreds = item['ingredients'] if "ingredients" in item else []
     directions = item['directions'] if "directions" in item else []
+    dirarr = ""
+    for i in directions:
+        dirarr += i + "$#"
     calories = item['calories'] if 'calories' in item else -1.0
     description = item['desc'] if 'desc' in item else ""
     for ingred in ingreds:
         temp = [rid, idCount, title, ingred]
         dataT2.append(collections.OrderedDict(zip(headerT2, temp)))
         rid += 1
-    arr = [idCount,title, rating, directions, calories, description]
+    arr = [idCount,title, rating, dirarr, calories, description]
     dataT1.append(collections.OrderedDict(zip(headerT1, arr)))
     idCount += 1
 
-
-with open("./outputData/recipes.json", 'w') as file:
+#
+with open("../outputData/recipes.json", 'w') as file:
     json.dump(dataT1, file, indent=2)
 
-with open("./outputData/ingredients.json", 'w') as file:
+with open("../outputData/ingredients.json", 'w') as file:
     json.dump(dataT2, file, indent=2)
 
