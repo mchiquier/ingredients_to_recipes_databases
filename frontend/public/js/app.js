57b6159115e9faf81d892a2b8977d61e2cb28d91
Andre Wallace <waandre@seas.upenn.edu>
2018-12-09 19:56:51 -0600

    finished basic frontend

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -1,13 +1,12 @@
-var app = angular.module('angularjsNodejsTutorial',['ngTable']);
+var app = angular.module('angularjsNodejsTutorial',[]);
 
-app.controller('myController', function($scope, $http, $window, $location) {
+app.controller('myController', function($scope, $http, $window) {
     var index = 0;
     $scope.Submit = function() {
         if ($scope.word === undefined || $scope.word.length == 0) {
             $window.location.reload()
         } else {
             var request = $http.get('/filterword/'+$scope.word);
-            $scope.word = ""
             request.success(function(data) {
                 $scope.data = data;
             });
@@ -42,16 +41,6 @@ app.controller('myController', function($scope, $http, $window, $location) {
             })
         }
     }
-
-    // $scope.recipeDetails = function(rec) {
-        
-    //     $window.location.href = '/recipe/' + rec.rid;
-    //     var request = $http.get('/recipe/:' + rec.rid)
-    //     // request.success(function(data) {
-    //         console.log(rec)
-    //     // })
-    // }
-
     $scope.allRecipes = function() {
         var request = $http.get('/fillhome/' + index)
         request.success(function(data) {
@@ -64,52 +53,57 @@ app.controller('myController', function($scope, $http, $window, $location) {
     $scope.tableParams = ({}, { dataset: $scope.data});
 });
 
-app.controller('recipe', function($scope, $http, $routeParams) {
-    var one = $routeParams.recipe
-    $scope.test = function() {
-        console.log("happening")
+app.controller('recipeController', function($scope, $http, $window) {
+    var url = $window.location.href.split('/')
+    var rid = url[url.length - 1]
+    $scope.recipeInfo = function() {
+        var request = $http.get('/recipe/info/' + rid)
+        request.success(function(data) {
+            $scope.title = data[0].title;
+            $scope.rating = data[0].rating != null ? data[0].rating : "Not yet rated";
+            var len = data[0].instructions.split('$#').length
+            $scope.instructions = data[0].instructions.split('$#').slice(0, len - 1);
+            $scope.cals = data[0].calories != null ? data[0].calories : "Unkown";
+            $scope.desc = data[0].description != "" ? data[0].description : "No available description";
+        })
+
+        var request1 = $http.get('/recipe/ingredients/' + rid)
+
+        request1.success(function(data) {
+            var conv = data[0].conversion
+            if (conv == null || conv == 0) {
+                conv = 1
+            }
+            conv = conv / 100
+            $scope.conv = conv
+            $scope.data = data
+            var energy = 0;
+            var fat = 0;
+            var carbs = 0;
+            var sugar = 0;
+            var fiber = 0;
+            var protein = 0;
+            var sodium = 0;
+            var potassium = 0;
+            for (var i = 0; i < data.length; i++) {
+                energy += (data[i].energy * conv)
+                fat += (data[i].fat * conv)
+                carbs += (data[i].carbohydrates * conv)
+                sugar += (data[i].sugar * conv)
+                fiber += (data[i].fiber * conv)
+                protein += (data[i].protein * conv)
+                sodium += (data[i].sodium * conv / 1000)
+                potassium += (data[i].potassium * conv / 1000) 
+            }
+            $scope.energy = energy
+            $scope.fat = fat
+            $scope.carbs = carbs
+            $scope.sugar = sugar
+            $scope.fiber = fiber
+            $scope.protein = protein
+            $scope.sodium = sodium
+            $scope.potassium = potassium
+        })
     }
-    console.log(one)
     
 });
-
-// app.controller('familyController', function($scope, $http) {
-//     $scope.message="";
-//     var selfInv = function() {
-//         console.log("happeneing")
-//         var request = $http.get('/familyLogins');
-//         request.success(function(data) {
-//             $scope.data = data;
-//         });
-//         request.error(function(data){
-//             console.log('err');
-//         });
-//     }();
-
-//     $scope.FindFamily = function() {
-//         console.log($scope.x)
-//         var request = $http.get('/family/'+$scope.login);
-//         request.success(function(data) {
-//             $scope.info = data;
-//         });
-//         request.error(function(data){
-//             console.log('err');
-//         });
-//     };
-// });
-
-
-// app.controller('insertController', function($scope, $http) {
-//     $scope.message="";
-//     $scope.Insert = function() {
-//         //json
-//         var request = $http.post('insertNew/'+$scope.login+'/'+$scope.name+'/'+$scope.sex+'/'+$scope.RelationshipStatus+'/'+$scope.Birthyear);
-//         request.success(function(data) {
-//             window.location = data.redirectURL
-//         });
-//         request.error(function(data){
-//             console.log('err');
-//         });
-
-//     }; 
-// });
--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -17,8 +17,29 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
 });
 
-router.get('/recipe/:recipe', function(req, res, next) {
+router.get('/recipe/:rid', function(req, res, next) {
+  // res.send({one: req.params.rid})
   res.sendFile(path.join(__dirname, '../', 'views', 'recipe.html'));
+});
+
+router.get('/recipe/info/:rid', function(req, res, next) {
+  var query = 'SELECT * FROM Recipe WHERE rid =' + req.params.rid;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }  
+  });
+});
+
+router.get('/recipe/ingredients/:rid', function(req, res, next) {
+  var query = 'SELECT * FROM Ingredient NATURAL JOIN Nutrient NATURAL JOIN Has WHERE rid =' + req.params.rid;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }  
+  });
 });
 
 router.get('/fillhome/:start', function(req, res, next) {
@@ -46,70 +67,5 @@ router.get('/filterword/:word', function(req,res) {
     }  
   });
 });
-
-// router.get('/reference', function(req, res, next) {
-//   res.sendFile(path.join(__dirname, '../', 'views', 'reference.html'));
-// });
-
-// router.get('/insert', function(req, res, next) {
-//   res.sendFile(path.join(__dirname, '../', 'views', 'insert.html'));
-// });
-
-// router.get('/friends', function(req, res, next) {
-//   res.sendFile(path.join(__dirname, '../', 'views', 'friends.html'));
-// });
-
-// router.get('/family', function(req, res, next) {
-//   res.sendFile(path.join(__dirname, '../', 'views', 'family.html'));
-// });
-
-// router.get('/family/:login', function(req, res, next) {
-//   var query = 'SELECT * FROM Person WHERE login IN (SELECT F.member FROM Family F WHERE F.login = \'' + req.params.login + '\')';
-//   connection.query(query, function(err, rows, fields) {
-//     if (err) console.log(err);
-//     else {
-//       console.log(rows)
-//       res.json(rows);
-//     }  
-//   })
-// });
-
-// router.get('/familyLogins', function(req, res, next) {
-//   var query = 'SELECT DISTINCT login FROM Family';
-//   connection.query(query, function(err, rows, fields) {
-//     if (err) console.log(err);
-//     else {
-//       res.json(rows);
-//     }  
-//   })
-// });
-
-
-
-// router.get('/friends/:email', function(req,res) {
-//   if (req.params.email) {
-//     var query ='SELECT * FROM Person P WHERE P.login IN (SELECT F.friend FROM Friends F WHERE F.login = \'' 
-//     + req.params.email + '\' OR (F.login IN (SELECT Fam.member FROM Family Fam WHERE Fam.login = \''
-//      + req.params.email + '\'))) AND P.login <> \'' + req.params.email + '\''; 
-
-//      connection.query(query, function(err, rows, fields) {
-//         if (err) console.log(err);
-//         else {
-//           res.json(rows);
-//         }  
-//       });
-//   } 
-// });
-
-// router.post('/insertNew/:login/:name/:sex/:relation/:birth', function(req,res) {
-//   var insert = 'INSERT INTO Person VALUES(\'' + req.params.login + '\',\'' + req.params.name + '\',\''
-//    + req.params.sex + '\',\'' + req.params.relation + '\',' + req.params.birth +')';
-//   connection.query(insert, function(err, rows, fields) {
-//     if (err) console.log(err);
-//     else {
-//       res.send({redirectURL: '/'})
-//     }
-//   });
-// });
 
 module.exports = router;
